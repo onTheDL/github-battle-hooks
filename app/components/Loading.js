7b48e26de463bcf5baabe9a8cbf9c75725f5f51e
Daniel Laserna <daniel.laserna@protonmail.com>
2021-05-27 19:10:57 -0400

    Refactor Loading.js to functional component with hooks

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,46 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const styles = {
   content: {
-    fontSize: '35px',
-    position: 'absolute',
-    left: '0',
-    right: '0',
-    marginTop: '20px',
-    textAlign: 'center',
-  }
-}
+    fontSize: "35px",
+    position: "absolute",
+    left: "0",
+    right: "0",
+    marginTop: "20px",
+    textAlign: "center",
+  },
+};
 
-export default class Loading extends React.Component {
-  state = { content: this.props.text }
-  componentDidMount () {
-    const { speed, text } = this.props
+export default function Loading({ speed = 300, text = "Loading" }) {
+  const [content, setContent] = useState(text);
 
-    this.interval = window.setInterval(() => {
-      this.state.content === text + '...'
-        ? this.setState({ content: text })
-        : this.setState(({ content }) => ({ content: content + '.' }))
-    }, speed)
-  }
-  componentWillUnmount () {
-    window.clearInterval(this.interval)
-  }
-  render() {
-    return (
-      <p style={styles.content}>
-        {this.state.content}
-      </p>
-    )
-  }
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      // content === `${text}...`
+      //   ? setContent(text)
+      //   : setContent((content) => `${content}.`);
+
+      setContent((content) => {
+        return content === text + "..."
+          ? text
+          : content + "."
+      })
+
+    }, speed);
+
+    return () => window.clearInterval(id);
+  }, [speed, text]);
+
+  return <p style={styles.content}>{content}</p>;
 }
 
 Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired,
-}
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
-}
+  text: PropTypes.string,
+  speed: PropTypes.number,
+};
